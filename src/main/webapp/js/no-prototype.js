cf38891510316d3e12f2fb6246418d568cc63768
Ulli Hafner <ullrich.hafner@gmail.com>
2019-12-12 22:16:34 +0100

    Fix some additional JQuery 2/3 shading problems.

--- src/main/webapp/js/no-prototype.js
+++ src/main/webapp/js/no-prototype.js
@@ -1,26 +1,17 @@
-/* global jQuery, Prototype */
-(function ($) {
-    /**
-     * Solves Bootstrap and Prototype.js conflict.
-     *
-     * @link http://jsfiddle.net/dgervalle/hhBc6/
-     * @link http://www.softec.lu/site/DevelopersCorner/BootstrapPrototypeConflict
-     */
-    jQuery.noConflict();
-    if (Prototype.BrowserFeatures.ElementExtensions) {
-        const disablePrototypeJS = function (method, pluginsToDisable) {
-            const handler = function (event) {
-                event.target[method] = undefined;
-                setTimeout(function () {
-                    delete event.target[method];
-                }, 0);
-            };
-            pluginsToDisable.each(function (plugin) {
-                jQuery(window).on(method + '.bs.' + plugin, handler);
-            });
+/* global Prototype, jQuery3 */
+if (Prototype.BrowserFeatures.ElementExtensions) {
+    const disablePrototypeJS = function (method, pluginsToDisable) {
+        const handler = function (event) {
+            event.target[method] = undefined;
+            setTimeout(function () {
+                delete event.target[method];
+            }, 0);
         };
-        const pluginsToDisable = ['collapse', 'dropdown', 'modal', 'tooltip', 'popover', 'tab'];
-        disablePrototypeJS('show', pluginsToDisable);
-        disablePrototypeJS('hide', pluginsToDisable);
-    }
-})(jQuery);
+        pluginsToDisable.each(function (plugin) {
+            jQuery3(window).on(method + '.bs.' + plugin, handler);
+        });
+    };
+    const pluginsToDisable = ['collapse', 'dropdown', 'modal', 'tooltip', 'popover', 'tab'];
+    disablePrototypeJS('show', pluginsToDisable);
+    disablePrototypeJS('hide', pluginsToDisable);
+}
